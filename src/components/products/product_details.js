8544d100e2846ace21cc5c6d493dcd3419fd1500
azmeirovitz <arcomp123@gmail.com>
2019-09-24 15:38:23 -0700

    Ariel Meirovitz sweet-corner v2-feature-set-4

--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {getProductDetils} from '../../actions';
+import Money from '../general/money';
+import './product-details.scss';
 
 class ProductDetails extends Component {
 
+componentDidMount () {
+    //console.log("Product Details Component Mounted");
+    //console.log("Product ID: ", this.props.match.url);
+
+    const {getProductDetils, match: {params}} = this.props;
+
+    getProductDetils(params.product_id);
+
+    // this.props.getProductDetils(this.props.match.params.product_id);
+
+}
+
+//A match object contains information about how a <Route path> matched the URL. It was products/id, and the id is the number that is part of the URL.
 
 
 render () {
 
+    //const {details} = this.props;
+    const {products} = this.props;
+
+    console.log("product details: ", this.props.products);
+
+    if (products == null)
+        return (<h1>Loading Page</h1>)
+
+    // if (!details){
+    //     return (
+    //     <h1>Loading Page</h1>
+    //     )
+    // }
     return (
 
-        <div className="product-details">
-            <h1>Product Details</h1>
+        <div className="product-details"> 
+
+            <h1 className="h1">{products.name}</h1> 
+            <img className="productImgage" src={products.thumbnail.url} alt="Cupcake" />
+            <h3 className="h3">Description:</h3>
+            <h4 className="h4">{products.description}</h4>
+            <div className="cost">
+            <Money cost={products.cost}/>
+            </div>
+
+                       
         </div>
 
     )
@@ -17,4 +56,18 @@ render () {
 
 }
 
-export default ProductDetails;
+function mapStateToProps (state) {
+    console.log("mapStateToProps state of product details: ", state);
+    return {
+        products: state.products.details
+    };
+}
+
+//Connecting Product component and getProductDetails to Redux:
+export default connect(mapStateToProps, {
+    getProductDetils: getProductDetils
+})(ProductDetails);
+
+                
+
+// export default ProductDetails;
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -10,6 +10,12 @@ export default (state = DEFAULT_STATE, action) => {
     switch(action.type){
         case types.GET_ALL_PRODUCTS:
             return {...state, list: action.products};
+        break;
+
+        case types.GET_PRODUCT_DETAILS:
+            return {...state, details:action.product};
+        break;
+
         default:
             return state;
     }
