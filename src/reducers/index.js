dafe9e5afd2a2126d2e5fedfd40b799d55c3a74d
azmeirovitz <arcomp123@gmail.com>
2019-09-25 22:03:49 -0700

    Ariel Meirovitz sweet-corner v3-feature-set-2

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import productReducer from './products_reducer';
+import cartReducer from './cart_reducer';
 
 
 const rootReducer = combineReducers({
     form: formReducer,
-    products: productReducer
+    cart: cartReducer,
+    products: productReducer    
 });
 
 export default rootReducer;
